d954d2cb1446c67394696a71b6b85c31a99381af
note-5i\shurik <abashev.alexandr@gmail.com>
2017-11-03 21:05:48 +0200

    final splash + dialog

--- SplashDialog/app/src/main/java/com/example/shurik/splashdialog/MainActivityDialog.java
+++ SplashDialog/app/src/main/java/com/example/shurik/splashdialog/MainActivityDialog.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by shurik on 03.11.2017.
@@ -16,6 +18,7 @@ public class MainActivityDialog extends Dialog implements View.OnClickListener{
     private MainActivity activity;
 
     private Button buttonOk;
+    private ImageView imageView;
 
     public MainActivityDialog(@NonNull Context context) {
         super(context);
@@ -26,11 +29,18 @@ public class MainActivityDialog extends Dialog implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setCanceledOnTouchOutside(false);
+        getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
 
         setContentView(R.layout.dialog_main_activity);
 
         buttonOk    = (Button) findViewById(R.id.dialog_main_activity_button_ok);
         buttonOk.setOnClickListener(this);
+
+        imageView   = (ImageView) findViewById(R.id.dialog_main_activity_image_view);
+        imageView.setImageResource(R.mipmap.ic_launcher_round);
 
     }
 
